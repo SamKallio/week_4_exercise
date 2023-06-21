720b0ebb920a5090ac65952c79e3e687f37b166f
SamKallio <sam.kalliomaki@gmail.com>
2023-06-21 15:52:04 +0300

    first commit

---
+++ public/javascripts/front.js
@@ -0,0 +1,145 @@
+if (document.readyState !== "loading") {
+  console.log("Document is ready");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document ready after waiting!");
+  });
+}
+
+let ingredientsList = [];
+let instructionsList = [];
+
+const inputName = document.getElementById("name-text");
+const inputImage = document.getElementById("image-input");
+const ingredientButton = document.getElementById("add-ingredient");
+const instructionButton = document.getElementById("add-instruction");
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", submitRecipe);
+ingredientButton.addEventListener("click", addIngredient);
+instructionButton.addEventListener("click", addInstruction);
+
+function addIngredient() {
+  const ingredientText = document.getElementById("ingredients-text");
+  ingredientsList.push(ingredientText.value);
+  console.log("Current ingredients: " + ingredientsList);
+}
+
+function addInstruction() {
+  const instructionsText = document.getElementById("instructions-text");
+  instructionsList.push(instructionsText.value);
+  console.log("Current instructions: " + instructionsList);
+}
+
+function submitRecipe() {
+  if (inputImage.value) {
+    let newForm = new FormData();
+    for (let index = 0; index < inputImage.files.length; index++) {
+      newForm.append("images", inputImage.files[index]);
+      console.log(newForm);
+    }
+
+    //Submit image if there is image
+    fetch("http://localhost:3000/images", {
+      method: "POST",
+      body: newForm,
+    })
+      .then((response) => response.text())
+      .then((data) => console.log(data));
+  }
+  if (
+    ingredientsList.length < 1 ||
+    instructionsList.length < 1 ||
+    inputName.value === ""
+  ) {
+    console.log("You haven't filled all the fields yet");
+    return;
+  }
+  const newRecipe = {
+    name: inputName.value,
+    ingredients: ingredientsList,
+    instructions: instructionsList,
+  };
+
+  fetch("http://localhost:3000/recipe", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newRecipe),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        displayRecipe(data);
+      } else {
+        console.log("Something went wrong when trying to fetch recipe!");
+      }
+    });
+
+  clearFields();
+}
+
+function initializeCode() {
+  const clearImages = document.getElementById("image-input");
+  clearImages.value = "";
+
+  fetch("http://localhost:3000/recipe/Pizza")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        displayRecipe(data);
+      } else {
+        console.log("Something went wrong when trying to fetch recipe!");
+      }
+    });
+}
+
+function displayRecipe(recipe) {
+  const recipeName = document.createElement("h1");
+  const ingredientTitle = document.createElement("h4");
+  const instructionTitle = document.createElement("h4");
+  const recipeInstructions = document.createElement("ul");
+  const recipeIngredients = document.createElement("ul");
+  const viewRecipe = document.getElementById("view-recipe");
+
+  recipeInstructions.style.listStyle = "decimal";
+
+  recipeName.innerText = recipe.name;
+  ingredientTitle.innerText = "Ingredients";
+  instructionTitle.innerText = "Instructions";
+
+  viewRecipe.append(recipeName);
+  viewRecipe.append(ingredientTitle);
+  viewRecipe.append(recipeIngredients);
+  viewRecipe.append(instructionTitle);
+  viewRecipe.append(recipeInstructions);
+
+  for (let index = 0; index < recipe.instructions.length; index++) {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(recipe.instructions[index]));
+    recipeInstructions.appendChild(li);
+    li.style.listStyle = "decimal";
+  }
+  for (let index = 0; index < recipe.ingredients.length; index++) {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(recipe.ingredients[index]));
+    recipeIngredients.appendChild(li);
+  }
+}
+
+function clearFields() {
+  //Clears lists
+  instructionsList = [];
+  ingredientsList = [];
+
+  //Clear name
+  inputName.value = "";
+
+  //Clear textareas
+  const textBoxes = document.querySelectorAll("textarea");
+  textBoxes.forEach((element) => {
+    element.value = "";
+  });
+}
---
+++ routes/recipe.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+
+// [
+//   {
+//     "name": "Pizza",
+//     "instructions": ["Cook first bro"],
+//     "ingredients": ["Potato"]
+//   }
+// ]
+
+const recipes = [
+  {
+    name: "Pizza",
+    instructions: ["Cook first", "Then serve"],
+    ingredients: ["Cheese", "Tomatoes", "Basil"],
+  },
+];
+
+router.get("/:food", function (req, res) {
+  console.log(req.params.food);
+  const getFood = recipes.find((food) => food.name === req.params.food);
+  console.log(getFood);
+  res.send(getFood);
+});
+
+router.post("//", (req, res) => {
+  recipes.push(req.body);
+  const getFood = recipes.find((food) => food.name === req.body.name);
+
+  if (getFood) {
+    res.send(getFood);
+    console.log("Works");
+  } else {
+    res.send(
+      "We couldn't add the food properly. Check the data you sent into the server that it is in correct format!"
+    );
+  }
+});
+
+module.exports = router;
